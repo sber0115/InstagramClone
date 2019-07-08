d31f81ab299b45e78960fff4cf27dc9fda118370
Sebastian Bernal <sbernal@andrew.cmu.edu>
2019-07-08 15:30:18 -0700

    Initial Commit

---
+++ InstagramClone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstagramClone
+//
+//  Created by sbernal0115 on 7/8/19.
+//  Copyright © 2019 sbernal0115. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InstagramClone/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InstagramClone
+//
+//  Created by sbernal0115 on 7/8/19.
+//  Copyright © 2019 sbernal0115. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
